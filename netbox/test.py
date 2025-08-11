d935f985f12d511585c437d852b5eec25b9e2ecc
harpy5 <94404826+harpy5@users.noreply.github.com>
2025-08-11 10:45:45 -0400

    Create test.py

---
+++ netbox/test.py
@@ -0,0 +1,5 @@
+from netbox_tools import connect_to_server, get_device_info_by_name, get_device_by_role
+from pyats_tools import show_interface_info_tor
+
+if __name__ = "__main__":
+  print(get_device_by_role("netinfra-tor-pe"))
